41a5c868bb6291af8f5dc848174ca6c53eeff112
jsepucas <jsepucas@myuax.com>
2023-12-14 12:23:22 +0100

    16

---
+++ 2_GestionMaterias.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Definición estructura de estudiante
+
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<std::string> materias;
+};
