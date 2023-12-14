9f0b0a2e99e46918d2d0ff96118894a87a9c4f12
jsepucas <jsepucas@myuax.com>
2023-12-14 12:49:13 +0100

    33 ??

--- 3_RegistroAsistenciaEnC.cpp
+++ 3_RegistroAsistenciaEnC.cpp
@@ -3,3 +3,14 @@
 
 #define MAX_MATERIAS 10
 #define MAX_ASISTENCIAS 30
+
+// Enumeración para representar los diferentes estados de asistencia
+
+typedef enum { Asistio, Falta, Tardanza } Estado;
+
+// Estructura para almacenar la información de asistencia
+struct Asistencia {
+    char fecha[10];
+    char materia[50];
+    Estado estado;
+};
