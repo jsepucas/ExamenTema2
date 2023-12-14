d27f703c7162efb0c901d9c459d351aefd574cdc
jsepucas <jsepucas@myuax.com>
2023-12-14 13:01:41 +0100

    40

--- 4_ManejoExcepciones.cpp
+++ 4_ManejoExcepciones.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <stdexcept> // Para std::runtime_error
-
-// Excepción personalizada por si hay errores de formato de fecha
+#include <stdexcept>
 
 class FechaFormatException : public std::runtime_error {
 public:
     FechaFormatException(const std::string& msg) : std::runtime_error(msg) {}
 };
 
+class MateriaNoRegistradaException : public std::runtime_error {
+public:
+    MateriaNoRegistradaException(const std::string& msg) : std::runtime_error(msg) {}
+};
 
+class Asistencia {
+public:
+    std::string fecha;
+    std::string materia;
+    enum Estado { Asistio, Falta, Tardanza } estado;
+
+    Asistencia(const std::string& f, const std::string& m, Estado e) : fecha(f), materia(m), estado(e) {}
+};
